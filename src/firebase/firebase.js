935147e107da8c005193af371b962688cc98d3ed
PRATHAM MITTAL <prathamv0106@gmail.com>
2025-03-10 20:05:03 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyD3RfyZ-ZxYq6Y6Obv8toZqQi1GiFKtg8Q",
+  authDomain: "basic-login-auth-e6f6b.firebaseapp.com",
+  projectId: "basic-login-auth-e6f6b",
+  storageBucket: "basic-login-auth-e6f6b.firebasestorage.app",
+  messagingSenderId: "1057614429135",
+  appId: "1:1057614429135:web:fe9bd4f85f10743a1170ba",
+  measurementId: "G-PVMC6Z4LHV"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
