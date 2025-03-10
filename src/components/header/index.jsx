e89d18abfffefdb528fbee21274cd8f16930a0b8
PRATHAM MITTAL <prathamv0106@gmail.com>
2025-03-10 20:07:34 +0530

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     const navigate = useNavigate()
     const { userLoggedIn } = useAuth()
     return (
-        <nav className='flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
+        <nav className='flex flex-row gap-x-5 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
             {
                 userLoggedIn
                     ?
@@ -17,7 +17,7 @@ const Header = () => {
                     :
                     <>
                         <Link className='text-sm text-blue-600 underline' to={'/login'}>Login</Link>
-                        <Link className='text-sm text-blue-600 underline' to={'/register'}>Register New Account</Link>
+                        <Link className='text-sm text-blue-600 underline' to={'/register'}>Register</Link>
                     </>
             }
 
